74bf0cfa53dc5d4a616889e79206987cd7c59d95
Your Name <you@example.com>
2026-04-13 22:37:29 +0700

    Auto-backup trước khi Deploy - 20260413_223728

---
+++ test_ftp.py
@@ -0,0 +1,15 @@
+import ftplib
+FTP_HOST = "51.79.53.190"
+FTP_USER = "alotoinghe"
+FTP_PASS = "Nghia234!"
+
+try:
+    ftp = ftplib.FTP(FTP_HOST)
+    ftp.login(FTP_USER, FTP_PASS)
+    ftp.cwd("/wp-content/themes/tehi-theme")
+    items = []
+    ftp.retrlines('LIST', items.append)
+    for i in items: print(i)
+    ftp.quit()
+except Exception as e:
+    print("Error:", e)
